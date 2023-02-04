fe12ee222e7c3f8477d31b3b96eb3062278ba5cb
SuperCoolGamer <103328500+SuperCoolGamer@users.noreply.github.com>
2023-02-03 21:13:05 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ function setup(){
     canvas = createCanvas(280,280);
     canvas.position(620,320);
     background("white");
-    
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis
 }
 
 function update_canvas(){
@@ -20,27 +21,43 @@ function update_canvas(){
     random_number=Math.floor(Math.random()*quick_draw_data_set.length);
     console.log(quick_draw_data_set[random_number]);
     document.getElementById("sketch_drawn").innerHTML="Sketch to be drawn: " + quick_draw_data_set[random_number];
-}
+} 
+
+
 
 function draw(){
+
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+
    check_sketch();
         if(sketch_drawn == your_sketch){
      answer_holder="set";
      score=score+1;
      document.getElementById("score").innerHTML = "Score:" + score;
-        
-    }  
+          }  
 
+
+    
 }
+
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
 
 function check_sketch(){
 timer_counter= timer_counter+1;
 document.getElementById("timer").innerHTML= "Timer: " + timer_counter;
-console.log(timer_counter)
+console.log(timer_counter);
 if(timer_counter>400){
     timer_counter=0;
     timer_check="completed";
-
 }
 if(timer_check=="completed" || answer_holder=="set"){
 timer_check="";
@@ -48,4 +65,24 @@ answer_holder="";
 update_canvas();
 }
 
+
+
+}
+
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('your_sketch').innerHTML = 'Your Sketch is ' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence*100)+"%";
+
+  utterThis = new SpeechSynthesisUtterance(results[0].label);
+  synth.speak(utterThis);
 }
